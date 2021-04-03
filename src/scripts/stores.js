f62dba55304e02c06bcdfc2f4fdaaf513e4cbca2
Niclas Kaufmann <kaufmann.niclas@web.de>
2021-04-03 21:38:39 +0200

    Added fancy animation

--- src/scripts/stores.js
+++ src/scripts/stores.js
@@ -1,12 +1,28 @@
-import { writable, derived } from 'svelte/store';
+import { writable, derived, get } from 'svelte/store';
 
 export const questions = writable([]);
 
 export const counter = derived(questions, ($questions) => {
+  const oldCounter = get(counter); 
+  const newCorrect = $questions.filter((question) => question.isCorrect).length
+  const newIncorrect = $questions.filter((question) => question.isCorrect === false).length;
+  let increased = undefined;
+  if (oldCounter) {
+    if (newCorrect > oldCounter.correct) {
+      increased = 'correct';
+    } else if (newIncorrect > oldCounter.incorrect) {
+      increased = 'incorrect';
+    }
+  } else if (newCorrect) {
+    increased = 'correct';
+  } else if (newIncorrect) {
+    increased = 'incorrect';
+  }
+    
   return {
     questions: $questions.length,
-    correct: $questions.filter((question) => question.isCorrect).length,
-    incorrect: $questions.filter((question) => question.isCorrect === false)
-      .length,
+    correct: newCorrect,
+    incorrect: newIncorrect,
+    increased
   };
 });
